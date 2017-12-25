3e17a6504eb75cf21a1f1334cf88db4f03f5fd3a
vpopovich <33244876+vpopovich@users.noreply.github.com>
2017-12-25 21:20:49 +0700

    Update Lab3_10.c

--- Lab3_10.c
+++ Lab3_10.c
@@ -1,17 +1,17 @@
 #include "stdafx.h"
 
-//---- возвращается индекс скобки " {" для пары с максимальной глубиной
+	//---- возвращается индекс скобки " {" для пары с максимальной глубиной
 
 void initString(char c[]);
 int findBrace(char c[]);
 int findEnd(char c[], int begin);
-void extractSubstring(char c[],char b[], int begin, int end);
+void extractSubstring(char c[], char b[], int begin, int end);
 void printResult(char c[], char b[]);
 
 void main()
 {
 	char c[255];
-	char b[255] = {'\0'};
+	char b[255] = { '\0' };
 	initString(c);
 	extractSubstring(c, b, findBrace(c), findEnd(c, findBrace(c)));
 	printResult(c, b);
@@ -23,27 +23,22 @@ void initString(char c[])
 }
 int findBrace(char c[])
 {
-	int i;                                          // Индекс в строке
 	int k = 0;                                         // Счетчик вложенности
-	int max;                                    // Максимум вложенности
-	int b;                                         // Индекс максимальной " {"
-	for (i = 0, max = 0, b = -1; c[i] != 0; i++)
+	int max = 0;                                    // Максимум вложенности
+	int index = -1;                                         // Индекс максимальной " {"
+	for (int i = 0; c[i] != 0; i++)
 	{
 		if (c[i] == '}')
 		{
 			k--;
 		}
-		if (k < 0)
-		{
-			return 0;
-		}
 		if (c[i] == '{')
 		{
 			k++;
-			if (k>max)
+			if (k > max)
 			{
 				max = k;
-				b = i;
+				index = i;
 			}
 		}
 	}
@@ -51,7 +46,7 @@ int findBrace(char c[])
 	{
 		return 0;                        // Защита " от дурака" , нет парных скобок
 	}
-	return b;
+	return index;
 }
 int findEnd(char c[], int begin)
 {
@@ -65,7 +60,7 @@ int findEnd(char c[], int begin)
 void extractSubstring(char c[], char b[], int begin, int end)
 {
 	int i = 0;
-	for ( i = 0; i < end - begin - 1 && c[i] != '\0'; i++)
+	for (i = 0; i < end - begin - 1 && c[i] != '\0'; i++)
 	{
 		b[i] = c[i + begin + 1];
 	}
@@ -74,7 +69,7 @@ void extractSubstring(char c[], char b[], int begin, int end)
 		c[begin + i] = c[end + 1 + i];
 	}
 }
-void printResult(char c[], char b[]) 
+void printResult(char c[], char b[])
 {
 	printf("%s\n", c);
 	printf("%s\n", b);
